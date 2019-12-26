9fed4c03b5b77209752a817aa98b01a040ee4b4a
Felipe Amaral de Souza <amaral.felipe@apoioprodesp.sp.gov.br>
2019-12-26 16:46:58 -0300

    Add Struct example

--- teste.go
+++ teste.go
@@ -6,4 +6,20 @@ import (
 
 func main() {
 	fmt.Println("Hello World")
+	type Character struct {
+		name string
+		age  int
+	}
+
+	red := Character{
+		name: "Red",
+		age:  12,
+	}
+
+	blue := Character{
+		name: "Blue",
+		age:  13,
+	}
+
+	fmt.Println(red, blue)
 }
