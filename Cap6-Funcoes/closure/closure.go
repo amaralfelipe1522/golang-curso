01e8e832569c96fd0c936df4ce9c4247d1dd04a0
Felipe Amaral de Souza <amaral.felipe@apoioprodesp.sp.gov.br>
2020-01-28 09:49:16 -0300

    Adding closure.go

---
+++ Cap6-Funcoes/closure/closure.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func closureEx() func() {
+	x := 10
+	printX := func() {
+		fmt.Println(x)
+	}
+	return printX
+}
+
+func main() {
+	x := 20
+	fmt.Println(x)
+
+	callFunc := closureEx()
+	callFunc()
+}
