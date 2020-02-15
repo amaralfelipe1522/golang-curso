de05801ada83b19f5b2c59311e08ca4744d3e6b8
amaralfelipe1522 <amaral.felipe@live.com>
2020-02-15 10:19:10 -0300

    Adding update in reuso/main.go file

--- Cap8-Pacotes-Visibilidade/reuso/main.go
+++ Cap8-Pacotes-Visibilidade/reuso/main.go
@@ -3,30 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/amaralfelipe1522/golang-area"
-	/*
-		package area
-
-		import "math"
-
-		//Pi é uma proporção númerica definida pela relação entre o perímetro de uma circunferência e seu diâmetro
-		const Pi = 3.1416
-
-		//Circ é responsável por calcular a área
-		func Circ(raio float64) float64 {
-			return math.Pow(raio, 2) * Pi
-		}
-
-		//Rect é responsável por calcular a área de um retangulo
-		func Rect(base, altura float64) float64 {
-			return base * altura
-		}
-
-		//_TrianguloEq é privada, não será visível em outro pacote
-		func _TrianguloEq(base, altura float64) float64 {
-			return (base * altura) / 2
-		}
-	*/)
+	area "github.com/amaralfelipe1522/golang-area"
+	//	go get github.com/amaralfelipe1522/golang-area.index
+)
 
 func main() {
 	fmt.Println(area.Circ(6.0))
