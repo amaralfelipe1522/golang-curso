64ab2ccbe54b0228dabba74239b66be2f0f7704b
amaralfelipe1522 <amaral.felipe@live.com>
2020-02-15 11:19:34 -0300

    Adding cpus.go and goroutine.go files

---
+++ Cap9-Concorrencia/goRoutines/goroutine.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func fale(pessoa, texto string, qtd int) {
+	for i := 0; i < qtd; i++ {
+		time.Sleep(time.Second)
+		fmt.Printf("%s: %s (iteranção de número %d)\n", pessoa, texto, i+1)
+	}
+}
+
+func main() {
+	//Executa de forma sequencial (serial), sem concorrência:
+	// fale("João", "Ô diabo", 4)
+	// fale("João2", "HEHEHE", 4)
+
+	//Criando uma Go Routine
+	go fale("João", "Ô diabo", 4)
+	go fale("João2", "HEHEHE", 4)
+	time.Sleep(time.Second * 3) //Go Routine terá 3 segundos para executar antes da função Main ser encerrada
+}
---
+++ Cap9-Concorrencia/teste.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	//Concorrência vs Paralelismo
+	//Paralelismo = Fazendo 2 coisas exatamente ao mesmo tempo, isso é possível usando 2 processadores
+	//Concorrencia = Capacidade de administrar múltiplas tarefas em um único processador
+}
