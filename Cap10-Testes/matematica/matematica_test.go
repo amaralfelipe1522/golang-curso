b5fe18ea7c72a2b6233cac3d099fc3395bdefe61
amaralfelipe1522 <amaral.felipe@live.com>
2020-03-08 19:10:23 -0300

    Adding matematica.go and matematica_test.go files

---
+++ Cap10-Testes/matematica/matematica_test.go
@@ -0,0 +1,22 @@
+package matematica
+
+// No terminal é possível executar o teste com o comando: go test
+// Ou em um diretório pai: go test ./...
+import (
+	"fmt"
+	"testing"
+)
+
+const erroPadrao = "Valor esperado %v, mas o resultado encontrado foi %v."
+const sucessoPadrao = "Valor esperado foi retornado."
+
+func TestMedia(t *testing.T) {
+	valorEsperado := 7.29
+	valor := Media(7.2, 9.9, 6.1, 5.9)
+
+	if valor != valorEsperado {
+		t.Errorf(erroPadrao, valorEsperado, valor)
+	} else {
+		fmt.Println(sucessoPadrao)
+	}
+}
